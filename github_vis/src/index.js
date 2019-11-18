4d8602b4b63c9d35dbdac835d728de5c5b78d30c
Owen Johnston <32434575+owjoh@users.noreply.github.com>
2019-11-18 13:23:01 +0000

    Added a simple text field for entering repository name, which will then be searched using the github api

--- github_vis/src/index.js
+++ github_vis/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SignIn from './SignIn'
 import * as serviceWorker from './serviceWorker';
 
 var element = React.createElement('h1', { className: 'greeting' }, 'hello world!');
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<SignIn />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
